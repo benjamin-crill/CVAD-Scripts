d8ee40ba28254836b99df7d80f94fb66d99fed5d
benjamin-crill <97719215+benjamin-crill@users.noreply.github.com>
2022-05-06 17:13:11 -0500

    Add files via upload

---
+++ ShutdownDeliveryGroup.ps1
@@ -0,0 +1,10 @@
+﻿asnp citrix.*
+
+$delGroup = Read-Host -Prompt 'Enter delivery group to shutdown'
+$broker = "name of your broker here"
+
+Set-BrokerDesktopGroup -AdminAddress $broker -Name $delGroup -InMaintenanceMode $true
+
+$desktops = Get-BrokerDesktop -DesktopGroupName $delGroup
+
+New-BrokerHostingPowerAction -MachineName $desktops.MachineName -Action Shutdown
